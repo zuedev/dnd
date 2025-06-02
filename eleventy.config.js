b3e70a07758785e0e071769bf4d6a4a62568d6b6
Alex Pooley (@zuedev) <zuedev@gmail.com>
2025-06-02 15:54:26 +0100

    fix: streamline global data in eleventy.config.js and set default values for title and background in default.html

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,13 +1,5 @@
 import { HtmlBasePlugin } from "@11ty/eleventy";
 
-const globalData = {
-  layout: "default.html",
-  title: "D&D Player Primer",
-  background_image:
-    "https://media.dndbeyond.com/compendium-images/phb/MKDHZ1nxSXDDLOw2/phb-2024-cover.jpeg",
-  background_position: "center",
-};
-
 export default function (eleventyConfig) {
   eleventyConfig.addPlugin(HtmlBasePlugin);
 
@@ -23,9 +15,7 @@ export default function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("source/_assets");
 
-  Object.entries(globalData).forEach(([key, value]) => {
-    eleventyConfig.addGlobalData(key, value);
-  });
+  eleventyConfig.addGlobalData("layout", "default.html");
 
   return {
     dir: {
